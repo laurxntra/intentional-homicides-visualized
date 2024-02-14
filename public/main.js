660261b51d6ae0fd2c1c74d75fabe409205dafd9
Tasawar Hannan <hannan25@cs.washington.edu>
2024-02-13 16:41:57 -0800

    edit tooltip

--- public/main.js
+++ public/main.js
@@ -48,9 +48,9 @@ d3.csv("data/ecsData/intentionalHomicides.csv").then(function(data) {
       .style("opacity", 0)
       .attr("class", "tooltip")
       .style("background-color", "white")
-      // .style("border", "solid")
-      // .style("border-width", "1px")
-      // .style("border-radius", "5px")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
       .style("padding", "2px")
 
     var mouseover = function(e, d) {
